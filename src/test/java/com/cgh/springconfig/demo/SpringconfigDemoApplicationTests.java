e00ca96f29576fafbc0ad20f77c344003e0a8f18
chen <540458161@qq.com>
2020-11-23 19:38:51 +0800

    第一次提交

---
+++ src/test/java/com/cgh/springconfig/demo/SpringconfigDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgh.springconfig.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringconfigDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
